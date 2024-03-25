442d2c874852ff5ed02c8920909486b9d0748836
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2024-03-25 14:31:45 -0300

    Add files via upload

---
+++ idade.cpp
@@ -0,0 +1,31 @@
+	# include <stdio.h>
+	#include <locale.h>
+	
+	int idade1,idade2;
+	
+	main(void){
+		
+		setlocale(LC_ALL,"Portuguese");
+		
+		printf("Informe a idade da primeira pessoa: ");
+		scanf("%i", &idade1);
+		
+		printf("Informe a idade da segunda pessoa: ");
+		scanf("%i", &idade2);
+		
+		if (idade1>idade2){
+			
+			printf("A primeira pessoa é mais velha!");
+		}else{
+			
+			printf("A segunda pessoa é mais velha!");
+		}
+		
+				
+	}
+	
+	
+	
+	
+	
+	
