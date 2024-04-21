0a696ac0dfc205fb001ec8001624fdbe1cba88ef
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2024-04-21 18:02:34 -0300

    Add files via upload

---
+++ expressao.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	
+	//variavel  = expressão;
+	// isso quer dizer a variável sempre recebe a expressão;
+	
+	int x = 5 y;
+	y = x;
+	y = x + 10;
+	
+	system ("pause");
+	return 0;
+	
+}
